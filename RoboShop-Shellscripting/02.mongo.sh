bfd8ebfcebb4a7a3d3e277582a76cebc4c2cbd30
Gopiraj <gopiraj0208@gmail.com>
2024-07-25 09:11:56 +0530

    MongoDB updated

--- RoboShop-Shellscripting/02.mongo.sh
+++ RoboShop-Shellscripting/02.mongo.sh
@@ -1,4 +1,8 @@
-cd /etc/yum.repos.d/mongo.repo
+[mongodb-org-7.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/9/mongodb-org/7.0/x86_64/
+enabled=1
+gpgcheck=0
 dnf install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
